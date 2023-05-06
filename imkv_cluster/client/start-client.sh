c6d648c550415565c6d53450e216e9c4afdcf602
joe0826 <18855009296@163.com>
2023-05-06 15:57:29 +0800

    update

--- imkv_cluster/client/start-client.sh
+++ imkv_cluster/client/start-client.sh
@@ -10,12 +10,11 @@ CONFIG_PATH=../config/
 
 SLEEP_TIME=5s
 sleep ${SLEEP_TIME}
-#sudo spread -n localhost
+
 echo "Starting client [${PROXY_NAME}]..."
 
 screen -dmS pro
 screen -dmS ycsb
 
-#本来是开启容器启动，现在尝试改成远程启动
 screen -S pro -p 0  -X stuff "./client -v -p ${CONFIG_PATH} -o client ${PROXY_NAME} tcp://${PROXY_IP}:${PROXY_PORT}"$'\n'
 tail -f /dev/null
