f35933d41e32ea5f156962b55440fc3abcc2aa9a
joe0826 <18855009296@163.com>
2023-05-06 17:41:00 +0800

    =update new script

--- memcached_cluster/scripts/gen_workload3.cpp
+++ memcached_cluster/scripts/gen_workload3.cpp
@@ -2,37 +2,29 @@
 #include <stdlib.h>
 #define LENGTH 256
 
-//静态整型变量 inc_v =1, 即使函数执行完毕也不会被销毁，而是会一直保留。作用是保证每次生成随机字符串时候都使用rand种子不同。
 static int inc_v = 1;
-//生成指定长度的随机字符串
+
 void creat_str(char *s, int length)
 {
-    //srand设置随机数生成器的种子为inc_v，保证每次生成随机字符串时候，种子都不同。
+
     srand(inc_v);
-    //生成 33到122之间的随机数，并将其转换成ASCII对应的字符。存储到指针s所指向的数组中。
+
     for(int i = 0; i < length; i++)
     {
-        //33-122,'!'->'z'
         *(s + i) = rand() % 89 + 33;
-        // 检查随机生成的字符是否有单引号或反斜杠。如果有，将其替换成字符1，因为会影响后面字符串的拼接
         if(*(s + i) == '\'' || *(s + i) == '\\')
             *(s + i) = '1';
     }
-    //最后的位置加上字符串结束符\0
     s[length] = '\0';
-    //将随机数种子值添加1，保证下一次生成随机字符串时使用的种子不同
     inc_v++;
 }
 
 int main()
 {
-    // 指向file类型的指针变量 fin_load, 用于指向文件流
     FILE *fin_load, *fin_run, *fout_set, *fout_test;
     if(!(fin_load = fopen("ycsb_load3.load", "r")))
     {
-        //如果文件打开失败，则输出一行错误信息。
         printf("The source file *.load not exist.\n");
-        //终止程序
         exit(-1);
     }
 
